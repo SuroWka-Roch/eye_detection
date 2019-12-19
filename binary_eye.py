5dc754f6cee153f99872fda8814434fe05aa3b3c
SuroWka-Roch <wojtek@surowka.com>
2019-12-19 20:08:53 +0100

    Start of comand line arguments

--- binary_eye.py
+++ binary_eye.py
@@ -1,5 +1,6 @@
 import cv2 as cv
 
+
 def prog(img):
     """Wyliczenie średniej wartości piksela dla obrazu zbinaryzowanego. Wartość średnia"""
     p = 0
@@ -9,13 +10,28 @@ def prog(img):
     return (p/(img.shape[0]*img.shape[1]))
 
 
-
 def make_binary(img):
     pic_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
     p = prog(pic_gray)
 
-    t, small  = cv.threshold(pic_gray, p/4.5, 255, cv.THRESH_BINARY,)
+    t, small = cv.threshold(pic_gray, p/4.5, 255, cv.THRESH_BINARY,)
     t, big = cv.threshold(pic_gray, p/1.5, 255, cv.THRESH_BINARY,)
 
-    return small ,big
+    return small, big
+
+
+def save_files(small, big, name_small="small.png", name_big="big.png"):
+    cv.imwrite("./output/{}".format(name_small), small)
+    cv.imwrite("./output/{}".format(name_big), big)
+
+
+def read_files(name_small="small.png", name_big="big.png"):
+    small = cv.imread("./output/{}".format(name_small))
+    big = cv.imread("./output/{}".format(name_big))
+
+    big = cv.cvtColor(big, cv.COLOR_BGR2GRAY)
+    small = cv.cvtColor(small, cv.COLOR_BGR2GRAY)
+
+    print(big.shape)
+    return small, big
--- main.py
+++ main.py
@@ -5,12 +5,32 @@ import argparse
 import circles
 import binary_eye
 
-
+FILE_NAME = "./fig/01.png"
 
 print("helllo darknes my old friend.....")
-pic = cv.imread("./fig/01.png")
 
-male,duze = binary_eye.make_binary(pic)
+
+parser = argparse.ArgumentParser(description='Preprepare the eye biometry')
+parser.add_argument( '--binary','-b',help="Only generate binary files into output folder",action="store_true")
+
+args = parser.parse_args()
+
+pic = cv.imread(FILE_NAME)
+
+
+if args.binary:
+    print("Generating Binary files")
+    male,duze = binary_eye.make_binary(pic)
+    binary_eye.save_files(male,duze)
+    exit()
+
+try:
+    binary_eye.read_files()
+except Exception as e:
+    print(e)
+
+
+male,duze = binary_eye.read_files()
 
 circles.show_circles(pic, circles.find_circles(male))
 circles.show_circles(pic, circles.find_circles(duze))
